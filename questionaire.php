114568b152f79af5e85561a2dbe08e8303d374b7
Jaydi11a <geraldtrotman@fas.harvard.edu>
2014-06-28 21:07:05 -0400

    Added beginning stages of .php file

---
+++ questionaire.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title> Gerald Trotman </title>
+	
+	<style>
+	
+		h1 {
+			text-align:center;
+		}
+	
+		.container {
+			width:50%;
+			margin-bottom:50px;
+		}	
+		
+		img {
+			display:block;
+			margin:auto;
+			width:100%;
+		}
+	
+	</style>
+
+	
+</head>
+<body>
+
+	
+		<h1> Gerald Trotman </h1>
+		
+		<img src= >
+		
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p> I am a former Apple Technician with a background in Mathematics, Mechanical, and Electrical Engineering. My passion is product design and have been a TA in product design at MIT for the past 4 years. I am currently pursuing my masters in IT with a concentration in Software Engineering. </p>
+		
+		<h2>Why are you taking this course?</h2>
+		<p> I am taking this course for several reasons. I want to strengthen my understanding of back end web design. I want to deversify my programming knowledge. Mostly, I want to learn data scraping techniques in attempts of being able to manipulate data and represent it in interesting ways visually or otherwise. </p>
+		
+		<h2>What previous experience do you have with HTML/CSS?</h2>
+		<p> I honestly have 0 previous experience with HTML/CSS. I've learned about it only superficially through the course of pursuing other programming hobbies. </p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p> I have dabbled in C++, Java, Objective C, OCaml, Ruby, and Python </p>
+		
+		<h2>Have you worked with PHP before? If so, to what extent?</h2>
+		<p> I have not. </p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p> I am working on a Mac. </p>
+		
+		<h2>Anything else we should know?</h2>
+		<p> My main reason for pursuing my masters in Software Engineering at this point is to complement Hardware Engineering. I love working with microcontrollers such as Arduinos, Rasberry Pi's, XBees, etc and I was hoping to explore interfacing that with dynamic web apps. </p>
+	
+	</div>
+	
+	<script src=""></script>
+	
+</body>
+</html>
